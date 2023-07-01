1fc479f3414b8bd565d497c101afbfc1ebf9db33
Ahmed kamal salah <ak8051651@gmail.com>
2023-07-01 21:49:58 +0300

    last update

--- src/components/Banar.jsx
+++ src/components/Banar.jsx
@@ -8,12 +8,47 @@ import logo5 from "../assets/logo-5.svg";
 const Banar = () => {
   return (
     <>
-      <div className="container columns-3xs mx-auto flex items-center justify-between mt-48 gap-4">
-        <div><img src={logo1} alt="LOGO" className="md:w-36"/></div>
-        <div><img src={logo2} alt="LOGO" className="md:w-36"/></div>
-        <div><img src={logo3} alt="LOGO" className="md:w-36"/></div>
-        <div><img src={logo4} alt="LOGO" className="md:w-36"/></div>
-        <div><img src={logo5} alt="LOGO" className="md:w-36"/></div>
+      <div className="hidden md:block">
+        <div className="container columns-3xs mx-auto flex items-center justify-between mt-48 gap-4">
+          <div>
+            <img src={logo1} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo2} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo3} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo4} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo5} alt="LOGO" className="md:w-36" />
+          </div>
+        </div>
+      </div>
+
+      {/* mobile screen */}
+      <div className="md:hidden container row-auto mx-auto grid items-center justify-between mt-48 gap-6">
+        <div className="flex flex-row justify-between items-center gap-4 w-[90%] mx-auto">
+          <div>
+            <img src={logo1} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo2} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo3} alt="LOGO" className="md:w-36" />
+          </div>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-9 w-64 mx-auto">
+          <div>
+            <img src={logo4} alt="LOGO" className="md:w-36" />
+          </div>
+          <div>
+            <img src={logo5} alt="LOGO" className="md:w-36" />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/mainCon.jsx
+++ src/components/mainCon.jsx
@@ -5,31 +5,74 @@ import char2 from "../assets/Content Image.png";
 const MainCon = () => {
   return (
     <>
-      <div className="container mx-auto flex columns-auto justify-center items-center gap-40 mt-40">
-        <div className="">
-          <img src={char1} alt="" />
-        </div>
-        <div className="w-[556px] flex gap-y-7 flex-col">
-          <div className="text-5xl font-bold">Subscription index</div>
-          <div className="text-xl">
-            A daily dataset to keep you up to date on subscription market trends
-            and what's happening in your vertical.
+      <div className="hidden md:block ">
+        <div className="container mx-auto flex columns-auto justify-center items-center gap-40 mt-40 text-start">
+          <div className="">
+            <img src={char1} alt="" />
           </div>
-          <a href="#" className="text-indigo-700 font-bold">Learn more</a>
+          <div className="w-[556px] flex gap-y-7 flex-col">
+            <div className="text-5xl font-bold">Subscription index</div>
+            <div className="text-xl">
+              A daily dataset to keep you up to date on subscription market
+              trends and what's happening in your vertical.
+            </div>
+            <a href="#" className="text-indigo-700 font-bold">
+              Learn more
+            </a>
+          </div>
+        </div>
+
+        <div className="container mx-auto flex columns-auto justify-center items-center gap-40 mt-20">
+          <div className="w-[556px] flex gap-y-7 flex-col">
+            <div className="text-5xl font-bold">In-depth metrics</div>
+            <div className="text-xl">
+              Accurate, real-time reporting at your fingertips. Getting data has
+              never been easier.
+            </div>
+            <a href="#" className="text-indigo-700 font-bold">
+              Learn more
+            </a>
+          </div>
+          <div className="">
+            <img src={char2} alt="" />
+          </div>
         </div>
       </div>
 
-      <div className="container mx-auto flex columns-auto justify-center items-center gap-40 mt-20">
-        <div className="w-[556px] flex gap-y-7 flex-col">
-          <div className="text-5xl font-bold">In-depth metrics</div>
-          <div className="text-xl">
-            Accurate, real-time reporting at your fingertips. Getting data has
-            never been easier.
+      {/* mobile screen */}
+      <div className="md:hidden container mx-auto flex justify-center items-center mt-40 text-center">
+        <div className="w-[70%] flex flex-col-reverse">
+          <div className="mt-7">
+            <img src={char1} alt="" />
           </div>
-          <a href="#" className="text-indigo-700 font-bold">Learn more</a>
+          <div className="flex gap-y-7 flex-col">
+            <div className="text-4xl font-bold">Subscription index</div>
+            <div className="text-xl">
+              A daily dataset to keep you up to date on subscription market
+              trends and what's happening in your vertical.
+            </div>
+            <a href="#" className="text-indigo-700 font-bold">
+              Learn more
+            </a>
+          </div>
         </div>
-        <div className="">
-          <img src={char2} alt="" />
+      </div>
+
+      <div className="md:hidden container mx-auto flex justify-center items-center mt-40 text-center">
+        <div className="w-[70%] flex flex-col">
+          <div className="flex gap-y-7 flex-col">
+            <div className="text-5xl font-bold">In-depth metrics</div>
+            <div className="text-xl">
+              Accurate, real-time reporting at your fingertips. Getting data has
+              never been easier.
+            </div>
+            <a href="#" className="text-indigo-700 font-bold">
+              Learn more
+            </a>
+          </div>
+          <div className="mt-7">
+            <img src={char2} alt="" />
+          </div>
         </div>
       </div>
     </>
